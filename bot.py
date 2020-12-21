e082ce805fc86a0083dffaf197bbee6f71edeae0
boompow <gatewaymesfin@gmail.com>
2020-12-21 12:29:22 +0300

    make changes

--- bot.py
+++ bot.py
@@ -103,9 +103,9 @@ def check(update, context, override_lock=None):
 
 
 # Welcome a user to the chat
-def welcome(update, context, new_member):
+def welcome(update, context):
     """ Welcomes a user to the chat """
-    # new_member = update.message.new_chat_members[0]
+    new_member = update.message.new_chat_members[0]
     message = update.message
     chat_id = message.chat.id
     logger.info(
@@ -417,7 +417,7 @@ def empty_message(update, context):
                 return introduce(update, context)
             # Another user joined the chat
             else:
-                return welcome(update, context, new_member)
+                return welcome(update, context)
 
     # Someone left the chat
     elif update.message.left_chat_member is not None:
